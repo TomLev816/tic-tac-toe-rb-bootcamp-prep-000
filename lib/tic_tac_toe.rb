3091ddc60f77ecbfb519001343dc2c74ab1267ca
Thomas Lever <thomaslever508@gmail.com>
2018-07-09 16:04:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,3 +45,11 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
